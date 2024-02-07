b0772db9429101e1e58aef0aa9aced12f733d9a6
Terry Kerr <terry@idletea.net>
2024-02-07 12:38:53 -0500

    initial tofu

---
+++ tofu/ssh_keys.tf
@@ -0,0 +1,4 @@
+resource "vultr_ssh_key" "default" {
+  name    = "terry@racoon.idte.net"
+  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/lO7nCN37HyDumRBchSR0wSr/+s/q/BBCFZh103jD1 terry@racoon.idte.net"
+}
---
+++ tofu/terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    vultr = {
+      source = "vultr/vultr"
+      version = "~> 2.19"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.24"
+    }
+  }
+
+  backend "s3" {
+    region = "auto"
+    bucket = "opentofu"
+    key    = "infra/base.tf"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_s3_checksum            = true
+
+    endpoints = {
+      s3 = "https://a0e9a519a696e7d8c04087b2ea84dfba.r2.cloudflarestorage.com"
+    }
+  }
+}
