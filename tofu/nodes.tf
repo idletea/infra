0562442a881b056501da5020dc52355dd3fe61f9
Terry Kerr <terry@idletea.net>
2024-03-17 13:19:14 -0400

    add tag for lemur

--- tofu/nodes.tf
+++ tofu/nodes.tf
@@ -3,6 +3,7 @@ resource "vultr_instance" "node" {
   plan              = "vc2-2c-4gb"
   os_id             = 2136
   hostname          = "lemur.idte.net"
+  label             = "lemur.idte.net"
   enable_ipv6       = true
   firewall_group_id = vultr_firewall_group.k3s.id
   ssh_key_ids       = [vultr_ssh_key.default.id]
