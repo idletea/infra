ab84d72af9bad1717d9e1155d729dfb6cffbf707
Terry Kerr <terry@idletea.net>
2024-03-25 20:14:17 -0400

    fix it up

--- tofu/ssh_keys.tf
+++ tofu/ssh_keys.tf
@@ -1,4 +1,4 @@
 resource "vultr_ssh_key" "default" {
-  name    = "terry@racoon.idte.net"
-  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/lO7nCN37HyDumRBchSR0wSr/+s/q/BBCFZh103jD1 terry@racoon.idte.net"
+  name    = "terry@corvus.idte.net"
+  ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQD56rkPQMwc4oZLBtQKqvTPiO7KV3djssMaLVLQGaq terry@corvus.idte.net"
 }
